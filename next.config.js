b75bc94081e0e48c99cb395c82550be288245f72
Md Nazmul Bijoy <nazmulbijoy9105@gmail.com>
2026-02-19 16:22:00 +0600

    fix: point api.js to Render backend URL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-// module.exports = {
-//   experimental: {
-//     turbo: false,
-//   },
-// };
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
+module.exports = nextConfig;
